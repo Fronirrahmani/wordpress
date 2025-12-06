9dbc84504c782e539bdc63d5efe46438375ef814
Fronirrahmani <167862283+Fronirrahmani@users.noreply.github.com>
2025-12-06 16:49:15 +0100

    sssss
    sssss

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1 {
+            $(".page-tittle").addClass("sticky");
+
+        }else { 
+            $(".page-tittle").removeclass("sticky"))
+    })
+})
